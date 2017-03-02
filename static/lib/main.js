6e957d09847ff480094c92f87e2be53903be2cd9
Brock Webberman <btw6391@gmail.com>
2017-03-01 20:19:13 -0600

    Fixed hooks

--- static/lib/main.js
+++ static/lib/main.js
@@ -6,9 +6,12 @@ $(window).on('action:composer.submit', function(ev, data) {
     // data.action can be `topics.post`, `posts.reply` or `posts.edit`
     // data.composerData is the object that will be submitted to the server.
 
+    console.log("Composer submit.");
+
     // lets add our new field into the data to be submitted for new topics
     if (data.action === 'topics.post') {
         data.composerData.age = data.composerEl.find('#age').val();
-        console.log(data.composerData.age);
+        console.dir(data.composerData);
+        console.log("Topic posted.");
     }
 });
